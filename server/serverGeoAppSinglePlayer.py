07d4bcf816b53c51653c6ad558195ff4b757c294
FrancescoArtibani97 <artibani.1743611@studenti.uniroma1.it>
2021-03-12 12:26:11 +0100

    update of singlePlayer server (guaranteed non-repetition of answers)

--- server/serverGeoAppSinglePlayer.py
+++ server/serverGeoAppSinglePlayer.py
@@ -5,20 +5,6 @@ from flask_restful import Api,Resource,reqparse   #for parse request and replies
 app = Flask(__name__)
 api = Api(app) 
 
-
-'''import time
-start_time = time.time()
-
-
-lat, long, country,locality,city= retrieveGeoInfo()
-
-print("lat: "+lat, "long: "+long,"country: "+country, "locality: "+ locality, "city: "+city )
-
-print("----------------------------------")
-print(retrieveSimilarAnswers( country,locality))
-
-print("--- %s seconds ---" % (time.time() - start_time))'''
-
 trueAnswer=0
 falseAnsw=1
 parser = reqparse.RequestParser()
@@ -37,13 +23,13 @@ class GeoApp(Resource):    #Resource for use Crud op and other...
                          #must be setted to 1 if are required the theree other possible false Answer
         level = args['level']
         if(req == trueAnswer):
-            lat, long, country,city = retrieveGeoInfo(level,1)
+            lat, long, country,city = retrieveGeoInfo(level,1,None)
             return {"error":False, 'msg':"return true answer", 'lat':lat,'long':long, 'responseCountry':country,'responseCity':city }
         elif(req==falseAnsw):
             if(args['country'] and args['city']):
                 country = args['country']
                 city = args['city']
-                f_lat,f_long,false_country,false_city = retrieveGeoInfo(level,3) 
+                f_lat,f_long,false_country,false_city = retrieveGeoInfo(level,3,country) 
                 falseAnswers = retrieveSimilarAnswers(country,city,false_country,false_city)
                 #falseAnswers = retrieveSimilarAnswers( country,city)
                 return {'error':False, 'msg':"return false answers", 
@@ -54,32 +40,6 @@ class GeoApp(Resource):    #Resource for use Crud op and other...
             else:
                  return {"error":True, 'msg':"required country and city of true Answer"}
         
-        
-'''
-    def post(self):
-        parser.add_argument('req',type=int,required = True) 
-        parser.add_argument('who',type=int,required = True)
-        parser.add_argument('moveX',type=int,required = True)
-        parser.add_argument('moveY',type=int,required = True)
-        args = parser.parse_args() #parse the msg
-
-        req = args['req']
-        who = args['who']
-        moveY = args['moveY']
-        moveX = args['moveX']
-
-        parser.remove_argument('moveX')
-        parser.remove_argument('moveY')
-
-        if req == MOVE:
-            lastMove[who] = [moveX,moveY]
-            return {"error":False, 'msg':"MOVE MESSAGE", 'moveX':lastMove[who][0], 'moveY': lastMove[who][1]}
-            
-        else:
-            return{"error": True, 'msg':"'req' !=2}"}
-
-
-'''
 
 api.add_resource(GeoApp,"/")
 
