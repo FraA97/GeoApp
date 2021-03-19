ff314182dd9c522684cbffcf27621ea799fda8ed
Marco Cerino <cerino.marco97@gmail.com>
2021-03-19 18:48:13 +0100

    fixed textview not found bug

--- App/MapsProject/app/src/main/java/com/example/mapsproject/Multiplayer/Fragment/PollingNewLevelFragment.kt
+++ App/MapsProject/app/src/main/java/com/example/mapsproject/Multiplayer/Fragment/PollingNewLevelFragment.kt
@@ -39,8 +39,7 @@ class PollingNewLevelFragment: Fragment() {
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        val rootView =  inflater.inflate(R.layout.fragment_loading, container, false)
-        rootView.findViewById<TextView>(R.id.loading_tv).setText("Waiting server response")
+        val rootView =  inflater.inflate(R.layout.fragment_loading_view, container, false)
         return rootView
     }
 
